e5a2e2f478804a7019feb7c1111c3d7472ca0ba7
chrisiacovella <chris.iacovella@gmail.com>
2026-07-20 14:15:18 -0700

    original qm9 implementation.

---
+++ qm9_schnet_E_F_original/submit_all_slurm_jobs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.002_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.002_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.002_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.001_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.001_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.001_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0001_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0001_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0001_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps123_ds42_lr0.002_normFalse.sh
+sbatch submit_slurm_ps124_ds43_lr0.002_normFalse.sh
+sbatch submit_slurm_ps125_ds44_lr0.002_normFalse.sh
+sbatch submit_slurm_ps123_ds42_lr0.001_normFalse.sh
+sbatch submit_slurm_ps124_ds43_lr0.001_normFalse.sh
+sbatch submit_slurm_ps125_ds44_lr0.001_normFalse.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normFalse.sh
+sbatch submit_slurm_ps123_ds42_lr0.0001_normFalse.sh
+sbatch submit_slurm_ps124_ds43_lr0.0001_normFalse.sh
+sbatch submit_slurm_ps125_ds44_lr0.0001_normFalse.sh
