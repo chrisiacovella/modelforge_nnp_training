6a62dc89ab1520890e791cf6ca59e49e70f0bd84
chrisiacovella <chris.iacovella@gmail.com>
2026-07-27 11:44:35 -0700

    setting up other jobs with slight modifications to split and patience for E only

---
+++ qm9_schnet_E_normTrue_v3/submit_all_slurm_jobs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sbatch submit_slurm_ps123_ds42_lr0.001_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.001_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.001_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.001_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.001_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.001_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.001_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.001_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.001_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue.sh
