77e31b88ea23a3fda512b1ad603c2e6ceb77e5c5
chrisiacovella <chris.iacovella@gmail.com>
2026-09-17 11:53:06 -0700

    testing geom_qm9

---
+++ geom_qm9_schnet_E_2018/submit_all_slurm_jobs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbatch submit_slurm_ps123_ds42_lr0.0005_normTrue_lr_factor0.5_nfilters64.sh
+sbatch submit_slurm_ps124_ds43_lr0.0005_normTrue_lr_factor0.5_nfilters64.sh
+sbatch submit_slurm_ps125_ds44_lr0.0005_normTrue_lr_factor0.5_nfilters64.sh
